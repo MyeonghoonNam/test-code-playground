52fa1da0a9bb1721cca8903120a741f137ed29d4
MyeonghoonNam <codenmh0822@naver.com>
2024-09-05 19:56:12 +0900

    feat : react custom hook test

--- src/utils/common.spec.js
+++ src/utils/common.spec.js
@@ -35,3 +35,54 @@ describe('pick util 단위테스트', () => {
     expect(pick(obj)).toEqual({});
   });
 });
+
+describe('debounce', () => {
+  beforeEach(() => {
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
+
+    // 특정 시간에 대한 테스트 환경 설정 가능
+    // vi.setSystemTime(new Date(~))
+  });
+
+  it('특정 시간이 지난 후 함수가 호출된다.', () => {
+    const spy = vi.fn();
+    const debouncedFn = debounce(spy, 300);
+
+    debouncedFn();
+
+    vi.advanceTimersByTime(300);
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('연이어 호출해도 마지막 호출 기준으로 지정된 타이머 시간이 지난 경우에만 함수가 호출된다.', () => {
+    const spy = vi.fn();
+    const debouncedFn = debounce(spy, 300);
+
+    // 최초 호출
+    debouncedFn();
+
+    // 최초 호출 후 0.2초 후 호출
+    vi.advanceTimersByTime(200);
+    debouncedFn();
+
+    // 두번째 호출 후 0.1초 후 호출
+    vi.advanceTimersByTime(100);
+    debouncedFn();
+
+    // 세번째 호출 후 0.2초 후 호출
+    vi.advanceTimersByTime(200);
+    debouncedFn(); // 👈 4번째 호출
+
+    // 네번째 호출 후 0.3초 후 호출
+    // 최초 호출 후에 함수 호출 간격이 0.3초 이상 -> 네번째 호출이 유일
+    vi.advanceTimersByTime(300);
+
+    // 네번을 호출했지만 실제 spy함수는 단 한 번만 호출
+    expect(spy).toHaveBeenCalledTimes(1);
+  });
+});
